9d9012f73bca971417dc82af5e72ff7eec9a63ca
DT0814 <729742011@qq.com>
2018-01-25 23:34:38 +0800

    manage create

---
+++ manage/manage-mapper/src/main/java/cn/xawl/manage/dao/ItemCatMapper.java
@@ -0,0 +1,7 @@
+package cn.xawl.manage.dao;
+
+import cn.xawl.manage.pojo.ItemCat;
+import com.github.abel533.mapper.Mapper;
+
+public interface ItemCatMapper extends Mapper<ItemCat> {
+}
---
+++ manage/manage-web/src/main/java/cn/xawl/manage/controller/ItemCatController.java
@@ -0,0 +1,35 @@
+package cn.xawl.manage.controller;
+
+import cn.xawl.manage.pojo.ItemCat;
+import cn.xawl.manage.service.ItemCatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/item/cat")
+public class ItemCatController {
+    @Autowired
+    private ItemCatService itenCatService;
+
+    @GetMapping()
+    public ResponseEntity<List<ItemCat>> queryItemListByParentId(@RequestParam(value = "id", defaultValue = "0") Long parentId) {
+        try {
+            List<ItemCat> list = itenCatService.queryItemListByParentId(parentId);
+            if (null == list) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            } else {
+                return ResponseEntity.ok(list);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
